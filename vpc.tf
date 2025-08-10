2bee1ca6206e28f8aa7fa68758df53a3ae088682
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-08-10 10:10:59 -0500

    changing names

--- vpc.tf
+++ vpc.tf
@@ -7,73 +7,53 @@
 #        | |                  
 #        |_|                  
 
-#==============#
-# VPC Resource #
-#==============#
+#===================#
+# Main VPC Resource #
+#===================#
 
-resource "aws_vpc" "lab_vpc" {
-  cidr_block       = var.cidr_block
-  instance_tenancy = "default"
+resource "aws_vpc" "main_vpc" {
+  cidr_block            = var.vpc_cidr
+  enable_dns_support    = "true"
+  enable_dns_hostnames  = "true"
+  instance_tenancy      = "default"
+
 
   tags = {
-    Name        = "${local.name_prefix}-vpc"
+    Name = "${local.name_prefix}-vpc"
     Environment = var.env
   }
 }
 
-#=================#
-# Subnet Resource #
-#=================#
+#=============================#
+# Internet Gateway Resource   #
+#=============================#
 
-resource "aws_subnet" "lab_web_sn" {
-count = length(local.availability_zones)
+resource "aws_internet_gateway" "main_igw" {
 
-  vpc_id     = aws_vpc.lab_vpc.id
-  cidr_block = cidrsubnet(var.cidr_block, var.borrowed_bits, count.index)
-  availability_zone = local.availability_zones[count.index]
+  vpc_id   = aws_vpc.main_vpc.id
+  
+depends_on = [aws_vpc.main_vpc]
+
   tags = {
-    Name        = "${local.name_prefix}-${count.index + 1}-web"
-    Environment = var.env
+    Name = "${local.name_prefix}-igw"
   }
 }
 
-#==================#
-# Internet Gateway #
-#==================#
+#=========#
+# Subnets #
+#=========#
 
-resource "aws_internet_gateway" "lab_igw" {
-  vpc_id = aws_vpc.lab_vpc.id
+resource "aws_subnet" "app_sn" {
 
-  tags = {
-    Name        = "${local.name_prefix}-web-igw"
-    Environment = var.env
-  }
-}
-
-#=====================#
-# Default Route Table #
-#=====================#
-
-resource "aws_default_route_table" "lab_default_rt" {
-  default_route_table_id = aws_vpc.lab_vpc.default_route_table_id
-
-    route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.lab_igw.id
-    }
+  count                   = length(var.availability_zones)
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, var.borrowed_bits, count.index)
+  depends_on              = [aws_vpc.main_vpc]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 
   tags = {
-    Name        = "${local.name_prefix}-web-sn"
-    Environment = var.env
+    Name = "${local.name_prefix}-${count.index}-web-sn"
   }
-}
 
-#=========================#
-# Route Table Association #
-#=========================#
-
-resource "aws_route_table_association" "rta" {
-  count          = length(local.availability_zones)
-  subnet_id      = aws_subnet.lab_web_sn[count.index].id
-  route_table_id = aws_default_route_table.lab_default_rt.id
 }
